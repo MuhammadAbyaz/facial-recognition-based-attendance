4b85584eec3117624ad68839141f92b4bc96f25e
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2023-12-09 19:19:02 -0800

    added counter on image frame

--- image_capture.py
+++ image_capture.py
@@ -6,10 +6,16 @@ from db import bucket_name, supabase
 
 
 def capture_images(id):
-    video = cv2.VideoCapture(0)
+    x = 570
+    y = 45
+    font = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 3.5
+    text_color =(0, 0, 0)
     num = 1
+    video = cv2.VideoCapture(0)
     while True:
         _, frame = video.read()
+        cv2.putText(frame, str(num), (x, y), font, font_scale,text_color , 3)
         cv2.imshow("Image Capture", frame)
         if cv2.waitKey(1) & 0xFF == ord('n'):
             cv2.imwrite(f"{num}.jpg", frame)
@@ -18,6 +24,7 @@ def capture_images(id):
                 break
             num += 1
     upload_images(id)
+
 
 
 def upload_images(id):
